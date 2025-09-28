fe6d0342d230c59fb6b4db355c4f951da8eab121
anjali-tripathi1 <123200498+aman73078@users.noreply.github.com>
2025-09-28 13:09:12 +0530

    Education page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { AboutComponent } from './layout/components/about/about.component';
+import { EducationComponent } from './layout/components/education/education.component';
 
 export const routes: Routes = [
-    {path:'',redirectTo:'overview', pathMatch:'full'},
+    {path:'',redirectTo:'overview/about', pathMatch:'full'},
     {path:'overview', component:LayoutComponent,
         children:[
-            {path:'about', component:AboutComponent}
+            {path:'about', component:AboutComponent},
+            {path:'education', component:EducationComponent},
         ]
     }
 ];
